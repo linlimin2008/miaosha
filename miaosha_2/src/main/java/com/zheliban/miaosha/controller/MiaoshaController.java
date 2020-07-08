8cda173359e3140bfeb7cbff7d515f95d1e8794d
0cm <linlimin@zealfi.com>
2020-07-08 22:16:55 +0800

    3-3

---
+++ miaosha_2/src/main/java/com/zheliban/miaosha/controller/MiaoshaController.java
@@ -0,0 +1,15 @@
+package com.zheliban.miaosha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @功能:
+ * @项目名:miaosha_2
+ * @作者:0cm
+ * @日期:2020/7/89:41 下午
+ */
+@Controller
+@RequestMapping("/goods")
+public class MiaoshaController {
+}
