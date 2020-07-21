ab23f22b981ed0da71ba9b94550f102fcd845c75
0cm <linlimin@zealfi.com>
2020-07-21 14:38:51 +0800

    5-1

--- miaosha_2/src/main/java/com/zheliban/miaosha/controller/GoodsController.java
+++ miaosha_2/src/main/java/com/zheliban/miaosha/controller/GoodsController.java
@@ -1,6 +1,8 @@
 package com.zheliban.miaosha.controller;
 
+import com.alibaba.druid.util.StringUtils;
 import com.zheliban.miaosha.domain.MiaoshaUser;
+import com.zheliban.miaosha.redis.GoodsKey;
 import com.zheliban.miaosha.redis.RedisService;
 import com.zheliban.miaosha.service.GoodsService;
 import com.zheliban.miaosha.service.MiaoshaUserService;
@@ -10,7 +12,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.IWebContext;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.context.webflux.SpringWebFluxContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 
@@ -23,18 +32,33 @@ public class GoodsController {
     RedisService redisService;
     @Autowired
     GoodsService goodsService;
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
 
     /*
      * do_login的响应体设置了cookie，存到了浏览器里，to_list请求的时候带上了cookie
      * 请求的时候会带上cookie，可以取值
      * 有些手机客户端并不会把token放进cookie里传给我们的服务端，而是放到参数里面传递，为了兼容这种情况从Request里再取一下这个cookie
      */
-    @RequestMapping("/to_list")
-    public String to_list(Model model, MiaoshaUser user) {//
+    @RequestMapping(value = "/to_list",produces = "text/html")
+    @ResponseBody
+    public String to_list(Model model, MiaoshaUser user, HttpServletRequest request, HttpServletResponse response) {//
         model.addAttribute("user", user);
         List<GoodsVo> list = goodsService.listGoodsVo();
         model.addAttribute("goodsList", list);
-        return "goods_list"; //跳转到商品列表
+        String html = redisService.get(GoodsKey.getGoodsList,"",String.class);
+        if (!StringUtils.isEmpty(html)){
+            return html;
+        }
+        //手动渲染
+        //Sping5中SpringWebContext方法过时
+        IWebContext ctx =new WebContext(request,response,
+                request.getServletContext(),request.getLocale(),model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list",ctx);
+        if (!StringUtils.isEmpty(html)){
+            redisService.set(GoodsKey.getGoodsList,"",html);
+        }
+        return html; //跳转到商品列表
     }
 
     @RequestMapping("/to_detail/{goodsId}")
---
+++ miaosha_2/src/main/java/com/zheliban/miaosha/redis/GoodsKey.java
@@ -0,0 +1,15 @@
+package com.zheliban.miaosha.redis;
+
+/**
+ * @功能:
+ * @项目名:miaosha_2
+ * @作者:0cm
+ * @日期:2020/7/155:31 下午
+ */
+public class GoodsKey extends BasePrefix{
+
+    public GoodsKey(int exporeSeconds,String prefix) {
+        super(exporeSeconds,prefix);
+    }
+    public static GoodsKey getGoodsList = new GoodsKey(60,"gl");
+}
